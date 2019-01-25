bc242541de32850d3bae31dafacb6ff1d05e50a4
shaoyun.li <shaoyun.li@gmail.com>
2019-01-25 16:45:28 +0000

    [add] implement LayoutService.GetByRowColumn()

--- GeometricLayout/Models/RightTriAngle.cs
+++ GeometricLayout/Models/RightTriAngle.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace GeometricLayout.Models
 {
     public class RightTriangle
-    {
+    {        
+
         [Required]
         public int X1 { get; set; }
 
@@ -21,5 +24,10 @@ namespace GeometricLayout.Models
 
         [Required]
         public int Y3 { get; set; }
+
+        public List<Tuple<int, int>> ToVertexList()
+        {
+            return new List<Tuple<int, int>> { new Tuple<int, int>(X1, Y1), new Tuple<int, int>(X2, Y2), new Tuple<int, int>(X3, Y3) };
+        }
     }
 }
--- GeometricLayout/Services/LayoutService.cs
+++ GeometricLayout/Services/LayoutService.cs
@@ -1,13 +1,40 @@
-﻿using GeometricLayout.Interfaces;
+﻿using System;
+using GeometricLayout.Interfaces;
 using GeometricLayout.Models;
 
 namespace GeometricLayout.Services
 {
     public class LayoutService : ILayoutService
     {
+        private readonly static char[] rowIds = { 'F', 'E', 'D', 'C', 'B', 'A' };
+
         public RightTriangle GetByRowColumn(char row, int column)
         {
-            throw new System.NotImplementedException();
+            int rowIndex = Array.IndexOf(rowIds, row);
+            var triangle = new RightTriangle();
+
+            if (column % 2 == 0)
+            {
+                int columnPosition = column / 2;
+                triangle.X1 = columnPosition * 10 - 10;
+                triangle.Y1 = rowIndex * 10 + 10;
+                triangle.X2 = columnPosition * 10;
+                triangle.Y2 = rowIndex * 10;
+                triangle.X3 = columnPosition * 10;
+                triangle.Y3 = rowIndex * 10 + 10;
+            }
+            else
+            {
+                int columnPosition = (int)Math.Ceiling((double)column / 2);
+                triangle.X1 = columnPosition * 10 - 10;
+                triangle.Y1 = rowIndex * 10;
+                triangle.X2 = columnPosition * 10 - 10;
+                triangle.Y2 = rowIndex * 10 + 10;
+                triangle.X3 = columnPosition * 10;
+                triangle.Y3 = rowIndex * 10;                
+            }
+
+            return triangle;
         }
 
         public TriangleIndentifier GetByCoordinates(int v1X, int v1Y, int v2X, int v2Y, int v3X, int v3Y)
