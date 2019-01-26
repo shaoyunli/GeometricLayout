ff40a2509c7d73141bbaa30982c13c59b60c399f
shaoyun.li <shaoyun.li@gmail.com>
2019-01-26 11:54:56 +0000

    [add] add DI to Startup

--- GeometricLayout/Startup.cs
+++ GeometricLayout/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using GeometricLayout.Services;
+using GeometricLayout.Interfaces;
 
 namespace GeometricLayout
 {
@@ -25,6 +21,7 @@ namespace GeometricLayout
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<ILayoutService, LayoutService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
